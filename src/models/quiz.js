f39bdda34dccab411b261bf545c4a95bddfac228
bananestar <s.vanderlinden13@gmail.com>
2022-09-06 15:26:57 +0200

    models Quiz

---
+++ src/models/quiz.js
@@ -0,0 +1,24 @@
+const { Sequelize, DataTypes } = require('sequelize');
+
+//! Models Quiz
+//todo: quizID --> UUID
+//todo: name   --> string
+
+/**
+ *
+ * @param {Sequelize} sequelize
+ */
+module.exports = (sequelize) => {
+    const Quiz = sequelize.define('quiz', {
+        quizID: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true,
+        },
+        name: {
+            type: DataTypes.STRING(100),
+            allowNull: false,
+        },
+    });
+    return Quiz;
+};
