2338ea56c841f933d7b50de797f1d706ea499d51
bananestar <s.vanderlinden13@gmail.com>
2022-09-06 16:02:27 +0200

    set-up + relation + export

--- src/models/index.js
+++ src/models/index.js
@@ -9,5 +9,62 @@ const sequelize = new Sequelize(DB_DATABASE, DB_USERNAME, DB_PASSWORD, {
 });
 
 //! initialisation
-const db = {}; 
+const db = {};
 db.sequelize = sequelize;
+
+//! set-up des models
+
+db.User = require('./user')(sequelize)
+db.Quiz = require('./quiz')(sequelize)
+db.Score = require('./score')(sequelize)
+db.Theme = require('./theme')(sequelize)
+db.Question = require('./question')(sequelize)
+db.Reponse = require('./reponse')(sequelize)
+db.QuizQuestions = require('./quizQuestions')(sequelize)
+
+//! Relation
+
+//? User --> Score
+db.User.hasMany(db.Score, {
+	foreignKey: 'userID',
+	onDelete: 'CASCADE',
+	allowNull: false,
+})
+
+//? Quiz --> score
+db.Quiz.hasMany(db.Score, {
+	foreignKey: 'quizID',
+	onDelete: 'CASCADE',
+	allowNull: false,
+})
+
+//? Quiz --> QuizQuestions
+db.Quiz.hasMany(db.QuizQuestions,{
+	foreignKey: 'quizID',
+	onDelete: 'CASCADE',
+	allowNull: false,
+})
+
+//? Question --> QuizQuestions
+db.Question.hasMany(db.QuizQuestions,{
+	foreignKey: 'questionID',
+	onDelete: 'CASCADE',
+	allowNull: false,
+})
+
+//? theme --> Question
+db.Theme.hasMany(db.Question,{
+	foreignKey: 'themeID',
+	onDelete: 'CASCADE',
+	allowNull: false,
+})
+
+//? Question --> Reponse
+db.Question.hasMany(db.Reponse,{
+	foreignKey: 'questionID',
+	onDelete: 'CASCADE',
+	allowNull: false,
+})
+
+//! Export
+module.exports = db;
