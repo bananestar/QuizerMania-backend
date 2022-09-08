a0d2f3a5d00eaa4ed7b7c56573806905d71df36f
Bananestar <s.vanderlinden13@gmail.com>
2022-09-08 15:18:19 +0200

    router user basic

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,7 @@
+const userRouter = require('./user-router');
+
 const router = require('express').Router();
+
+router.use('/users', userRouter);
 
 module.exports = router;
---
+++ src/routes/user-router.js
@@ -0,0 +1,15 @@
+const userController = require('../controllers/user-controller');
+const authJWT = require('../middleware/auth-middleware');
+const bodyValidation = require('../middleware/body-validation-middleware');
+const { userUpdatedValidator } = require('../validators/user-validator');
+const userRouter = require('express').Router();
+
+userRouter.route('/').get(userController.getAll);
+
+userRouter
+	.route('/:id')
+	.get(userController.get)
+	.put(authJWT(), bodyValidation(userUpdatedValidator), userController.update)
+	.delete(authJWT(), userController.delete);
+
+module.exports = userRouter;
