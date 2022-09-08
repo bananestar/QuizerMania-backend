50d07def4c07c38430a6187bd227668cd1abd915
Bananestar <s.vanderlinden13@gmail.com>
2022-09-08 11:26:38 +0200

    controller score (getAll, get, add, update,delete)

--- src/controllers/score-controller.js
+++ src/controllers/score-controller.js
@@ -14,35 +14,87 @@ const scoreController = {
 	 * @param {Request} req
 	 * @param {Response} res
 	 */
-	getAll: async (req, res) => {},
+	getAll: async (req, res) => {
+		//Todo: recherche tout les scores dans la db
+		const data = await db.Score.findAndCountAll();
+		return res.status(200).json(new SuccessArrayResponse(data.rows, data.count));
+	},
 	//! recuperation d'un score spécifique via son ID
 	/**
 	 *
 	 * @param {Request} req
 	 * @param {Response} res
 	 */
-	get: async (req, res) => {},
+	get: async (req, res) => {
+		const scoreID = parseInt(req.params.id);
+		//Todo: recherche du score dans la db
+		const score = await db.Score.findOne({
+			where: { userID: scoreID },
+		});
+
+		//? cas si le quiz est introuvable ou n'existe pas
+		if (!score) {
+			return res.status(404).json(new NotFoundErrorResponse('Score not found'));
+		}
+
+		return res.status(200).json(new SuccessObjectResponse(score));
+	},
 	//! ajout d'un score
 	/**
 	 *
 	 * @param {Request} req
 	 * @param {Response} res
 	 */
-	add: async (req, res) => {},
+	add: async (req, res) => {
+		const data = req.validatedData;
+		//Todo: Ajout du score à la db
+		const newScore = await db.Score.create(data);
+		return res.status(201).json(new SuccessObjectResponse(newScore, 201));
+	},
 	//! mise à jour d'un score
 	/**
 	 *
 	 * @param {Request} req
 	 * @param {Response} res
 	 */
-	update: async (req, res) => {},
+	update: async (req, res) => {
+		const scoreID = parseInt(req.params.id);
+		const data = req.validatedData;
+
+		//Todo: request de mise à jour
+		const updatedScore = await db.Score.update(data, {
+			where: { scoreID },
+			returning: true,
+		});
+
+		if (!updatedScore[1]) {
+			return res.status(400).json(new ErrorResponse('BAD REQUEST'));
+		}
+
+		//Todo: request pour voir la modification
+		const updateValue = await db.Score.findOne({ where: { scoreID } });
+		return res.status(200).json(new SuccessObjectResponse(updateValue));
+	},
 	//! suppression d'un score
 	/**
 	 *
 	 * @param {Request} req
 	 * @param {Response} res
 	 */
-	delete: async (req, res) => {},
+	delete: async (req, res) => {
+		const scoreID = parseInt(req.params.id);
+		//Todo: request de suppression
+		const nbRow = await db.Score.destroy({
+			where: { scoreID },
+		});
+
+		//? cas Erreur: Quiz introuvable
+		if (nbRow !== 1) {
+			return res.status(404).json(new NotFoundErrorResponse('Score not found'));
+		}
+        
+		return res.sendStatus(204);
+	},
 };
 
 module.exports = scoreController;
