1f721d92a3b3298fad46d68c7ddaa876134abd3e
Bananestar <s.vanderlinden13@gmail.com>
2022-09-09 11:47:45 +0200

    add comment validator user

--- src/validators/user-validator.js
+++ src/validators/user-validator.js
@@ -1,11 +1,13 @@
 const yup = require('yup');
 
+//! validator form add user
 const userValidator = yup.object().shape({
 	pseudo: yup.string().trim().required().max(50),
 	email: yup.string().email().trim().required().max(70),
 	password: yup.string().trim().required().max(60),
 });
 
+//! validator form update user
 const userUpdatedValidator = yup.object().shape({
 	pseudo: yup.string().trim().max(50),
 	email: yup.string().email().trim().max(70),
