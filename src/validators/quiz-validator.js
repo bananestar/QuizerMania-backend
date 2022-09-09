543ef2b7c4a76fccdf75c23c33fbcd99e605d00d
Bananestar <s.vanderlinden13@gmail.com>
2022-09-09 14:38:41 +0200

    correction validator quiz

--- src/validators/quiz-validator.js
+++ src/validators/quiz-validator.js
@@ -2,12 +2,23 @@ const yup = require('yup');
 
 //! validator form add quiz
 const quizValidator = yup.object().shape({
+	quizID: yup.string().trim().required().uuid(),
 	name: yup.string().trim().required().max(100),
+	createdAt: yup.date().default(function () {
+		return new Date();
+	}),
+	updatedAt: yup.date().default(function () {
+		return new Date();
+	}),
 });
 
 //! validator form update quiz
 const quizUpdatedValidator = yup.object().shape({
+	quizID: yup.string().trim().required().uuid(),
 	name: yup.string().trim().max(100),
+	updatedAt: yup.date().default(function () {
+		return new Date();
+	}),
 });
 
 module.exports = { quizValidator, quizUpdatedValidator };
