0d5b259a5987665ea0b0b89754589c41d4d0bcf4
Bananestar <s.vanderlinden13@gmail.com>
2022-09-09 14:11:22 +0200

    validator score (add, update)

---
+++ src/validators/score-validator.js
@@ -0,0 +1,28 @@
+const yup = require('yup');
+
+//! validator form add score
+const scoreValidator = yup.object().shape({
+	scoreID: yup.string().trim().required().uuid(),
+	userID: yup.string().trim().required().uuid(),
+	quizID: yup.string().trim().required().uuid(),
+	score: yup.number().required().positive(),
+	createdAt: yup.date().default(function () {
+		return new Date();
+	}),
+	updatedAt: yup.date().default(function () {
+		return new Date();
+	}),
+});
+
+//! validator form update score
+const scoreUpdatedValidator = yup.object().shape({
+	scoreID: yup.string().trim().uuid(),
+	userID: yup.string().trim().uuid(),
+	quizID: yup.string().trim().uuid(),
+	score: yup.number().positive(),
+	updatedAt: yup.date().default(function () {
+		return new Date();
+	}),
+});
+
+module.exports = { scoreValidator, scoreUpdatedValidator}
